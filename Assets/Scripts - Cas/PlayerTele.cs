e3dd99729537b5ff5893d7fe12ecae21fb30213a
CasAmmerman <cassidyammerman02@gmail.com>
2025-09-08 18:17:03 -0400

    Teleportation Notes
    I began working on the teleportation mechanic and mapping out the plans in code.

---
+++ Assets/Scripts - Cas/PlayerTele.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerTele : MonoBehaviour
+{
+    //Player teleportation script 
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check if user presses the right mouse button at any point. 
+        //Check should also be done for any obsticals in the way such as walls, trees, ect.
+        //Players should NOT be able to teleport through walls unless a specific instense is encouraged. 
+        //Make sure all items that can not be teleported through contain a label to prevent the player from teleporting. 
+        //    Player Radius object is called 'PlayerRadius"
+
+    }
+}
