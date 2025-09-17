1d0763a7a8abb8309099e6d8d14c73ba8196e4cf
CasAmmerman <cassidyammerman02@gmail.com>
2025-09-17 18:16:56 -0400

    Game Art and Started Enemy Controls
    Began arranging the background and color composition. Began working on ground enemy code.

---
+++ Assets/Scripts - Cas/MouseMove.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MouseMove : MonoBehaviour
+{
+    
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
